d2099174d8b801774fdc5e1d771b78dfb71abad8
remikoutcherawy <remi.koutcherawy@gmail.com>
2019-08-22 14:04:36 +0200

    Add back texture, tests.

---
+++ app/src/androidTest/java/rk/or/android/View3DTest.java
@@ -0,0 +1,44 @@
+package rk.or.android;
+
+import android.app.Activity;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import rk.or.Commands;
+import rk.or.Model;
+
+import static org.junit.Assert.assertNotNull;
+
+public class View3DTest {
+    @Rule
+    public ActivityTestRule<ModelView> activityRule =
+            new ActivityTestRule<>(ModelView.class);
+
+    @Test
+    public void init() {
+        Activity activity = activityRule.getActivity();
+        assertNotNull(activity);
+
+        Looper.prepare();
+
+        ModelView modelView = (ModelView) activity;
+        View3D view3d = new View3D(modelView);
+        assertNotNull(view3d);
+
+        Model model = new Model();
+        model.init(-200, -200, 200, -200, 200, 200, -200, 200);
+        assertNotNull(model);
+
+        view3d.initBuffers(model);
+        assertNotNull(view3d);
+
+        view3d.onSurfaceCreated(null, null);
+        assertNotNull(view3d);
+    }
+
+}
